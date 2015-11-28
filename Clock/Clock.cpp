4f2cce02e866173f6f4aaa5ef35deafced08f98c
Lavrikova1997 <lavrikova1997@yandex.ru>
2015-11-28 16:10:08 +0400

    .

--- Clock/Clock.cpp
+++ Clock/Clock.cpp
@@ -5,6 +5,7 @@ const float RADIUS_COMMON_POINT = 1.f;
 const float RADIUS_HOUR_POINT = 3.f;
 const float ROTATE_ANGLE = 6.f;
 const float RADIUS_CIRCLE = 130.f;
+const int COUNT_POINT = 60;
 
 void iniClock(MyClock & clock)
 {
@@ -29,7 +30,7 @@ void drawCirclePoint(RenderWindow & window)
 {	
 	CircleShape DrawerPoint(RADIUS_COMMON_POINT);
 	DrawerPoint.setFillColor(Color(150, 150, 150));
-	for (int i = 0; i < 60; i++)
+	for (int i = 0; i < COUNT_POINT; i++)
 	{
 		if (i % 5 == 0)
 		{
